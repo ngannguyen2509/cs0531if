3d22466790f1ac3cbcd79124953de13c284f984e
hori akari <104044168+ruthia-1341@users.noreply.github.com>
2024-05-31 15:38:47 +0900

    練習8-1

--- cs0531if/Form1.cs
+++ cs0531if/Form1.cs
@@ -2,9 +2,17 @@ namespace cs0531if
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
